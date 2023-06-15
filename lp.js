5d882d95769ed9e981d5c24edfe36bebd1528f67
Reeann Chloe Albert <rcalbertofc@gmail.com>
2023-06-15 00:47:23 -0400

    added animated bar graph with css and js

--- lp.js
+++ lp.js
@@ -1,6 +1,22 @@
-const top = document.querySelector('.toTop');
-
-function goToTop{
-
-}
-
+setTimeout(function() {
+    $('.bar').each(function(i) {
+      var $bar = $(this);
+      $bar.append('<span class="count"></span>');
+      setTimeout(function() {
+        $bar.css('width', $bar.attr('data-percent'));
+      }, i * 100);
+    });
+  
+    $('.count').each(function() {
+      $(this).prop('Counter', 0).animate({
+        Counter: $(this).parent('.bar').attr('data-percent')
+      }, {
+        duration: 2000,
+        easing: 'swing',
+        step: function(now) {
+          $(this).text(Math.ceil(now) + '%');
+        }
+      });
+    });
+  }, 500);
+  
